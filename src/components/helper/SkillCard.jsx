1f1a20e27e6cafa52c1ebd145168373672813391
ramen72 <ramen@devsramen.com>
2025-03-20 17:26:07 +0600

    Update SkillCard.jsx

--- src/components/helper/SkillCard.jsx
+++ src/components/helper/SkillCard.jsx
@@ -3,8 +3,8 @@ import ArrowButton from './ArrowButton';
 
 const SkillCard = ({titleText,bodyText,btnText}) => {
     return (
-        <div className='group w-[370px] h-[334px] px-[50px] pt-[50px] pb-9 border border-stoke transition-all duration-300 hover:bg-radish'>
-            <div></div>
+        <div className='relative group w-[370px] h-[334px] px-[50px] pt-[50px] pb-9 border border-stoke transition-all duration-300 hover:bg-radish overflow-hidden'>
+            <div className='w-80 h-80 bg-gradient-to-r from-[#F46C88] to-[rgba(251,165,189,0.2)] rounded-full absolute -left-[73px] -top-[46px] opacity-0 transition-all duration-300 group-hover:opacity-100'></div>
             <div className='relative z-10'>
                 <h2 className='font-raleway font-medium text-2xl text-lightBlack capitalize mb-6 transition-all duration-300 group-hover:text-white'>{titleText}</h2>
                 <p className='font-raleway font-normal text-base text-lightBlack leading-6 mb-10 transition-all duration-300 group-hover:text-white'>{bodyText}</p>
